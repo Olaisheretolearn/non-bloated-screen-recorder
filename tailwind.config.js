855ac335bf4a4cc3bc2f51f90d2649b5719b7956
Olaisheretolearn <63797098+Olaisheretolearn@users.noreply.github.com>
2024-04-18 23:56:01 -0500

    i dont know the fuvk i am doing

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+    },
     extend: {},
   },
   plugins: [],
